3053ff124662b046e5aae7a0cb45120bc4a2a851
Johnny3kk <jjnew@mail.ru>
2020-03-13 14:33:27 +0300

    Hello home work 3

--- Phonebook.java
+++ Phonebook.java
@@ -1,29 +1,31 @@
-package ru.gb.jtwo.lthree.home;
+package home;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.stream.Collectors;
 
 public class Phonebook {
 
     private final HashMap<String, ArrayList<Person>> entries = new HashMap<>();
 
-    public void add(String name, String phone, String mail) {
+    public void add(String name, String phone, String email) {
         if (entries.containsKey(name)) {
             ArrayList<Person> persons = entries.get(name);
-            persons.add(new Person(phone, mail));
+            persons.add(new Person(phone, email));
         } else {
             ArrayList<Person> persons = new ArrayList<>();
-            persons.add(new Person(phone, mail));
+            persons.add(new Person(phone, email));
             entries.put(name, persons);
         }
     }
 
-    public ArrayList<String> getMails(String name) {
+    public ArrayList<String> getMails(String name){
         if (!entries.containsKey(name)) return null;
-        // РѕРїС‚РёРјРёР·Р°С†РёСЏ, С‡С‚Рѕ С‚С‹ РґРµР»Р°РµС€СЊ, РїСЂРµРєСЂР°С‚Рё)))
-        return entries.get(name).stream().map(person -> person.email)
-                .collect(Collectors.toCollection(ArrayList::new));
+        ArrayList<Person> persons = entries.get(name);
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 0; i < persons.size(); i++) {
+            result.add(persons.get(i).email);
+        }
+        return result;
     }
 
     public ArrayList<String> getPhones(String name) {
